96f83e52d7314e5c5489e0df8acea335527410fd
Alex Huszagh <ahuszagh@ahuszagh.localdomain>
2017-07-05 11:56:41 -0700

    Removed a bit more MPL-specific logic to abstract to std::tuple

--- boost/multi_index/detail/base_type.hpp
+++ boost/multi_index/detail/base_type.hpp
@@ -12,6 +12,7 @@
 #include <brigand/functions/arithmetic/identity.hpp>
 #include <boost/mpl/apply.hpp>
 #include <boost/mpl/at.hpp>
+#include <boost/mpl/fold.hpp>
 #include <boost/mpl/size.hpp>
 #include <boost/multi_index/detail/index_base.hpp>
 #include <boost/multi_index/detail/is_index_list.hpp>
@@ -38,21 +39,33 @@ struct index_applier
 };
 
 
-// TODO: I needto convert this to use sfinae logic...
+template < typename T, typename R >
+struct to_tuple;
 
-
-template <int N,typename V, typename A, typename I>
-struct nth_layer_impl
+template < typename... Ts, typename X >
+struct to_tuple< std::tuple< Ts... >, X >
 {
-  static const int length = mpl::size<I>::value;
+  typedef std::tuple< Ts..., X > type;
+};
+
+
+template <int N,typename... Ts>
+struct nth_layer_impl;
+
+
+template <int N,typename V, typename A, typename... Ts>
+struct nth_layer_impl<N, V, A, std::tuple<Ts...>>
+{
+  typedef std::tuple<Ts...> T;
+  static const int length = std::tuple_size<T>::value;
 
   typedef typename brigand::eval_if_c<
     N==length,
-    brigand::identity<index_base<V,I,A> >,
+    brigand::identity<index_base<V,  mpl::vector<Ts...>, A>>,
     mpl::apply2<
       index_applier,
-      mpl::at_c<I,N>,
-      nth_layer_impl<N+1, V, A, I>
+      std::tuple_element<N, T>,
+      nth_layer_impl<N+1, V, A, std::tuple<Ts...>>
     >
   >::type type;
 };
@@ -62,15 +75,21 @@ template <int N, typename... Ts>
 struct nth_layer;
 
 
-template <int N,typename V, typename A, typename I>
-struct nth_layer<N, V, A, I>
+template <int N,typename Value, typename Index, typename Allocator>
+struct nth_layer<N, Value, Index, Allocator>
 {
-  using type = typename nth_layer_impl<N, V, A, I>::type;
+  typedef typename mpl::fold<
+    Index,
+    std::tuple<>,
+    to_tuple<mpl::_1, mpl::_2>
+  >::type tuple;
+
+  using type = typename nth_layer_impl<N, Value, Allocator, tuple>::type;
 };
 
 
 template<typename Value,typename IndexSpecifierList,typename Allocator>
-struct multi_index_base_type: nth_layer<0,Value, Allocator, IndexSpecifierList>
+struct multi_index_base_type: nth_layer<0,Value, IndexSpecifierList, Allocator>
 {
   static_assert(detail::is_index_list<IndexSpecifierList>::value, "");
 };
